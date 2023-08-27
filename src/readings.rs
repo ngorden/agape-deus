357d870f02ba330f83c4cf4689baed2284170f77
ngorden <nick@nickgorden.xyz>
2023-08-27 10:14:21 -0700

    switch obsoleted `date()` with `date_naive()`

--- src/readings.rs
+++ src/readings.rs
@@ -1,11 +1,10 @@
+use crate::models::UniversalisResponse;
+use chrono::format::{DelayedFormat, StrftimeItems};
+use chrono::{Datelike, Duration, Local, Weekday};
+use regex::Regex;
 use std::error::Error;
 use std::ops::Add;
-use chrono::{Datelike, Duration, Local, Weekday};
-use chrono::format::{DelayedFormat, StrftimeItems};
-use regex::Regex;
 use substring::Substring;
-use crate::models::UniversalisResponse;
-
 
 const REGULAR_EXPRESSION: &str =
     "(<(div|span) style=.\"(\\w|-|:| |%|;|#|\\.)+.\">)|</*\\w+>|(&#\\w+).;+";
@@ -13,13 +12,25 @@ const REGULAR_EXPRESSION: &str =
 pub async fn get_readings(next_sunday: bool) -> Result<(), Box<dyn Error>> {
     let today = Local::now();
 
-    let reading_string = format!("https://www.universalis.com/{}/jsonpmass.js", if next_sunday { get_next_sunday() } else { today.format("%Y%m%d") });
+    let reading_string = format!(
+        "https://www.universalis.com/{}/jsonpmass.js",
+        if next_sunday {
+            get_next_sunday()
+        } else {
+            today.format("%Y%m%d")
+        }
+    );
     let response = reqwest::get(reading_string).await?.text().await?;
     let reading_unformatted = response.substring("universalisCallback(".len(), response.len() - 3);
     let re = Regex::new(REGULAR_EXPRESSION).unwrap();
 
-    let mut clean = re.replace_all(&reading_unformatted, &String::from("")).to_string();
-    clean = Regex::new("&#8217;").unwrap().replace_all(&*clean, "'").to_string();
+    let mut clean = re
+        .replace_all(&reading_unformatted, &String::from(""))
+        .to_string();
+    clean = Regex::new("&#8217;")
+        .unwrap()
+        .replace_all(&*clean, "'")
+        .to_string();
     let reading_formatted: UniversalisResponse = serde_json::from_str(&*clean)?;
     println!("{}", reading_formatted);
 
@@ -28,13 +39,15 @@ pub async fn get_readings(next_sunday: bool) -> Result<(), Box<dyn Error>> {
 
 fn get_next_sunday<'a>() -> DelayedFormat<StrftimeItems<'a>> {
     let today = Local::now();
-    match today.weekday() {
-        Weekday::Sun => today.format("%Y%m%d"),
-        Weekday::Mon => today.date().add(Duration::days(6)).format("%Y%m%d"),
-        Weekday::Tue => today.date().add(Duration::days(5)).format("%Y%m%d"),
-        Weekday::Wed => today.date().add(Duration::days(4)).format("%Y%m%d"),
-        Weekday::Thu => today.date().add(Duration::days(3)).format("%Y%m%d"),
-        Weekday::Fri => today.date().add(Duration::days(2)).format("%Y%m%d"),
-        Weekday::Sat => today.date().add(Duration::days(1)).format("%Y%m%d"),
-    }
+    let days_to_add = match today.weekday() {
+        Weekday::Sun => Duration::days(0),
+        Weekday::Mon => Duration::days(6),
+        Weekday::Tue => Duration::days(5),
+        Weekday::Wed => Duration::days(4),
+        Weekday::Thu => Duration::days(3),
+        Weekday::Fri => Duration::days(2),
+        Weekday::Sat => Duration::days(1),
+    };
+
+    today.date_naive().add(days_to_add).format("%Y%m%d")
 }
